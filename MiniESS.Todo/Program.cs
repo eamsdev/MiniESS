b9cdf67d70061169234c2d1e0716f18fdc4dd52e
pete-eams <76290232+pete-eams@users.noreply.github.com>
2022-10-26 22:26:41 +1100

    Added NSwag interface generation support

--- MiniESS.Todo/Program.cs
+++ MiniESS.Todo/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
@@ -16,6 +17,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseOpenApi();
+app.UseSwaggerUi3();
 
 
 app.MapControllerRoute(
