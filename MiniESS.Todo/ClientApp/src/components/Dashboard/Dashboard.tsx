ea35e57b8c1f371b63330269dcd338a8c4c499be
pete-eams <76290232+pete-eams@users.noreply.github.com>
2022-11-12 16:57:40 +1100

    refactored to use react-bootstrap component

--- MiniESS.Todo/ClientApp/src/components/Dashboard/Dashboard.tsx
+++ MiniESS.Todo/ClientApp/src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,7 @@
 import { observer } from 'mobx-react';
 import { FunctionComponent } from 'react';
-import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import ButtonWithTextInput from '../../components-library/ButtonWithTextInput';
-import TodoList from '../../components-library/TodoList';
 import { TodoLists } from '../Todo/TodoLists';
 import { todoUiStore } from '../Todo/UiStore';
 
@@ -15,11 +14,9 @@ export const Dashboard: FunctionComponent = observer(() => {
           onSubmit={todoUiStore.domainStore.addNewTodoList}
         />
       </div>
-      <Container fluid>
-        <div className="row">
-          <TodoLists todoLists={todoUiStore.todoListsProps} />
-        </div>
-      </Container>
+      <Row>
+        <TodoLists todoLists={todoUiStore.todoListsProps} />
+      </Row>
     </>
   );
 });
