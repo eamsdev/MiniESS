a2385305bbf1b2f63193d1d0ebb874ad1c2a6380
eamsdev <76290232+pete-eams@users.noreply.github.com>
2023-01-04 21:28:53 +1100

    added scaffolding for Command Processor

---
+++ MiniESS.Core/Commands/CommandProcessor.cs
@@ -0,0 +1,30 @@
+﻿using MiniESS.Core.Aggregate;
+
+namespace MiniESS.Core.Commands;
+
+public interface ICommandProcessor
+{
+    Task ProcessAndCommit<T>(ICommand<T> command) where T : class, IAggregateRoot;
+}
+
+public class CommandProcessor : ICommandProcessor
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public CommandProcessor(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    
+    public Task ProcessAndCommit<T>(ICommand<T> command) where T : class, IAggregateRoot
+    {
+        // TODO:
+        // 1. Get Aggregate Type
+        // 2. Resolve Aggregate Repository from Service Provider 
+        // 3. Retrieve Aggregate from Repository, base on Aggregate Id
+        // 4. Via Reflection, get method for processing commands, call said method
+        // 5. AggregateRoot will need to inherit IProcessCommand<> which will emit event within
+        // 6. Persist
+        throw new NotImplementedException();
+    }
+}
---
+++ MiniESS.Core/Commands/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MiniESS.Core.Aggregate;
+
+namespace MiniESS.Core.Commands;
+
+public interface ICommand<TAggregateRoot> : ICommand 
+    where TAggregateRoot : class, IAggregateRoot
+{ }
+
+public interface ICommand
+{ 
+    public Guid AggregateId { get; init; }
+}
